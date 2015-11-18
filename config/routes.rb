a326f83ae3ba7fe7126f95025575084e8c6453be
mrjaimisra <misrajai01@gmail.com>
2015-11-18 10:43:09 -0700

    change 'log out/in' to 'sign out/in'

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
 
   namespace :users, path: ":user", as: :user do
     resources :links do
